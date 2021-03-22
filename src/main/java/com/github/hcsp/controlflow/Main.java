185af936d260c49a0112b07da44c02cdb86e950b
bujidefeng <1587770721@qq.com>
2021-03-22 20:32:01 +0800

    循环打印数字

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -7,9 +7,21 @@ public class Main {
      * <p>例如，start=1,end=5，则打印1,3,5 又如，start=-2,end=2，则打印-1,1
      *
      * @param start 区间开始
-     * @param end 区间结束
+     * @param end   区间结束
      */
-    public static void printOddNumbersBetween(int start, int end) {}
+    public static void printOddNumbersBetween(int start, int end) {
+        StringBuilder j = new StringBuilder();
+        for (int i = start; i <= end; i++) {
+            if (Math.abs(i % 2) == 1) {
+                if (i + 2 <= end) {
+                    j.append(i).append(",");
+                } else {
+                    j.append(i);
+                }
+            }
+        }
+        System.out.println(j);
+    }
 
     public static void main(String[] args) {
         printOddNumbersBetween(1, 5);
